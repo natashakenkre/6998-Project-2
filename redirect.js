7bd472f77f72f59e802f15f4897696a0490f0f5b
raphanus <phil.wpark+github@gmail.com>
2015-04-14 18:37:35 -0400

    merge

--- redirect.js
+++ redirect.js
@@ -1,7 +1,9 @@
 var http = require('http');
 var dao = require('./apis_manager_dao');
+var querystring = require('querystring');
+var request = require('request');
 
-function GEToptions(host, path, query, fields) {
+function options(host, path, method, id, query, fields) {
     var constructed_path = path;
 
     if (fields != null) {
@@ -12,94 +14,61 @@ function GEToptions(host, path, query, fields) {
         constructed_path += "?q=" + query;
     }
     
-    return {
-        host : host,
-        path: constructed_path,
-        method: 'GET',
+    var fin =  {
+        uri : host+'/'+constructed_path,
+        method: method,
         headers: {
             'Content-Type': 'application/json'
-        }
+        },
+        json: true
     }
-}
+    console.log(fin);
 
-function POSToptions(host, path, data) {
-    return {
-        host : host,
-        path: path,
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Countent-Length': data.length
-        }
-    }
-}
-
-function DELETEoptions(host, path) {
-    console.log(path);
-
-    return {
-        host : host,
-        path: path,
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }
+    return fin;
 }
 
 exports.getJSON = function(options, onResult) {
-    var req = http.request(options, function(res)
-    {
-        var output = '';
-        res.setEncoding('utf8');
-
-        res.on('data', function (chunk) {
-            output += chunk;
-        });
-
-        res.on('end', function() {
-            var obj = JSON.parse(output);
-            onResult(res.statusCode, obj);
-        });
+    request(options, function(error, response, body) {
+        if (error) {
+            throw(error)
+        }
+        if (!error && response.statusCode==200) {
+            console.log(body)
+            onResult(response.statusCode, body)
+        }
     });
-
-    req.on('error', function(err) {
-        console.log('error: ' + err.message);
-    });
-
-    req.end();
 };
 
 exports.postJSON = function(options, data, onResult)
 {
-    var req = http.request(options, function(res)
-    {
-        var output = '';
-        res.setEncoding('utf8');
-
-        res.on('data', function (chunk) {
-            output += chunk;
-        });
-
-        res.on('end', function() {
-            console.log(output);
-            var obj = JSON.parse(output);
-            onResult(res.statusCode, obj);
-        });
+    console.log(data);
+    var test_options = options;
+    test_options['body'] = data;
+    
+    request(test_options, function(error, response, body) {
+        if (error) {
+            console.log(error)
+        }
+        if (!error) {
+            console.log(body);
+            onResult(response.statusCode, body);
+        }
     });
-
-    req.on('error', function(err) {
-        console.log('error: ' + err.message);
-    });
-
-    req.write(data);
-
-    req.end();
 };
 
 
 exports.deleteJSON = function(options, itemId, onResult)
 {
+    request(options, function(error, response, body) {
+        if (error) {
+            throw(error)
+        }
+        if (!error) {
+            console.log(body);
+            onResult(response.statusCode, body);
+        }
+    });
+/*
     var req = http.request(options, function(res)
     {
         var output = '';
@@ -120,6 +89,7 @@ exports.deleteJSON = function(options, itemId, onResult)
     });
 
     req.end();
+    */
 };
 
 module.exports = {
@@ -129,9 +99,10 @@ module.exports = {
 
         for (index in managed_apis) {
             var api = managed_apis[index];
-
-            app.get('/' + api.id + '/:path', function (req, res) {
-                exports.getJSON(GEToptions(api.url, req.params.path, req.query.q, req.query.fields),
+            console.log(api);
+            app.get('/' + api.id + '/:path(*)', function (req, res) {
+                console.log(req)
+                exports.getJSON(options(api.url, req.params.path, 'GET', null, req.query.q, req.query.fields),
                     function (statusCode, result) {
                         console.log("onResult: (" + statusCode + ")" + JSON.stringify(result));
                         res.statusCode = statusCode;
@@ -166,6 +137,17 @@ module.exports = {
                     });
             });
 
+            app.put('/' + api.id + '/:path(*)', function (req, res) {
+                console.log("we in that put");
+                exports.postJSON(options(api.url, req.params.path, 'PUT'),
+                    req.body,
+                    function (statusCode, result) {
+                        console.log("onResult: (" + statusCode + ")" + JSON.stringify(result));
+                        res.statusCode = statusCode;
+                        res.send(result);
+                    });
+            });
+
             app.post('/' + api.id + '/:path', function (req, res) {
                 var data = JSON.stringify(req.body);
 
@@ -177,8 +159,9 @@ module.exports = {
                     });
             });
 
-            app.delete('/' + api.id + '/:path/:id', function (req, res) {
-                exports.deleteJSON(DELETEoptions(api.url, req.params.path + '/' + req.params.id),
+            app.delete('/' + api.id +'/:path(*)', function (req, res) {
+                console.log(req);
+                exports.deleteJSON(options(api.url, req.params.path, 'DELETE'),
                     '',
                     function (statusCode, result) {
                         console.log("onResult: (" + statusCode + ")" + JSON.stringify(result));
