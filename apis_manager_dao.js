8b081ecb0d7cc407c6b4613a193b4c61cfd03d74
Renato Vieira <guerin16@gmail.com>
2015-04-20 17:26:30 -0400

    Removing connection reconnect error

--- apis_manager_dao.js
+++ apis_manager_dao.js
@@ -16,7 +16,6 @@ function handleDisconnect() {
 
     connection.connect(function(err) {
         if(err) {
-            console.log('error when connecting to db:', err);
             setTimeout(handleDisconnect, 2000);
         }
     });
