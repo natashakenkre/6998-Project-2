72d38c0093455f7d18990215ab512dfa86a9a93f
Renato Vieira <guerin16@gmail.com>
2015-04-20 01:24:28 -0400

    Solving conflicts

--- redirect.js
+++ redirect.js
@@ -1,7 +1,7 @@
 var dao = require('./apis_manager_dao');
 var request = require('request');
 
-function options(host, path, method, query, fields) {
+function GEToptions(host, path, query, fields) {
     var constructed_path = path;
 
     if (fields != null) {
@@ -11,10 +11,10 @@ function options(host, path, method, query, fields) {
     if (query != null) {
         constructed_path += "?q=" + query;
     }
-    
+
     var fin =  {
         uri : 'http://'+host+'/'+constructed_path,
-        method: method,
+        method: 'GET',
         headers: {
             'Content-Type': 'application/json'
         },
@@ -77,8 +77,7 @@ exports.getJSON = function(options, onResult) {
             throw(error)
         }
         if (!error && response.statusCode==200) {
-            //console.log(body)
-            console.log('get');
+            console.log(body)
             onResult(response.statusCode, body)
         }
     });
@@ -87,14 +86,13 @@ exports.getJSON = function(options, onResult) {
 exports.postJSON = function(options, data, onResult)
 {
     console.log(data);
-    
+
     request(options, function(error, response, body) {
         if (error) {
             console.log(error)
         }
         if (!error) {
-            //console.log(body);
-            console.log('post');
+            console.log(body);
             onResult(response.statusCode, body);
         }
     });
@@ -116,85 +114,60 @@ exports.deleteJSON = function(options, itemId, onResult)
 
 module.exports = {
     initialize: function (app) {
-        dao.get_apis(function (res) {
-            managed_apis =  res;
-            var index;
+        var managed_apis = dao.get_apis();
+        var index;
 
-            for (index in managed_apis) {
-                var api = managed_apis[index];
-                console.log(api);
-                app.get('/' + api.id + '/:path(*)', function (req, res) {
-                    console.log(req)
-                    exports.getJSON(options(api.url, req.params.path, 'GET', null, req.query.q, req.query.fields),
-                        function (statusCode, result) {
-                            console.log("onResult: (" + statusCode + ")" + JSON.stringify(result));
-                            res.statusCode = statusCode;
-                            res.send(result);
-                        });
-                });
-
-                app.get('/' + api.id + '/:path&fields=:fields' , function (req, res) {
-                    exports.getJSON(options(api.url, req.params.path, req.query.q, req.params.fields),
-                        function (statusCode, result) {
-                            console.log("onResult: (" + statusCode + ")" + JSON.stringify(result));
-                            res.statusCode = statusCode;
-                            res.send(result);
-                        });
-                });
-
-                app.get('/' + api.id + '/:path/:id', function (req, res) {
-                    exports.getJSON(options(api.url, req.params.path + '/' + req.params.id, req.query.q, req.query.fields),
-                        function (statusCode, result) {
-                            console.log("onResult: (" + statusCode + ")" + JSON.stringify(result));
-                            res.statusCode = statusCode;
-                            res.send(result);
-                        });
-                });
-
-                app.get('/' + api.id + '/:path/:id&fields=:fields' , function (req, res) {
-                    exports.getJSON(options(api.url, req.params.path + '/' + req.params.id, req.query.q, req.params.fields),
-                        function (statusCode, result) {
-                            console.log("onResult: (" + statusCode + ")" + JSON.stringify(result));
-                            res.statusCode = statusCode;
-                            res.send(result);
+        for (index in managed_apis) {
+            var api = managed_apis[index];
+            console.log(api);
+            app.get('/' + api.id + '/:path(*)', function (req, res) {
+                console.log(req)
+                exports.getJSON(GEToptions(api.url, req.params.path, req.query.q, req.query.fields),
+                    function (statusCode, result) {
+                        console.log("onResult: (" + statusCode + ")" + JSON.stringify(result));
+                        res.statusCode = statusCode;
+                        res.send(result);
                     });
-                });
+            });
 
-                app.put('/' + api.id + '/:path(*)', function (req, res) {
-                    exports.postJSON(options(api.url, req.params.path, 'PUT'),
-                        req.body,
-                        function (statusCode, result) {
-                            console.log("onResult: (" + statusCode + ")" + JSON.stringify(result));
-                            res.statusCode = statusCode;
-                            res.send(result);
-                        });
-                });
+            app.get('/' + api.id + '/:path(*)&fields=:fields' , function (req, res) {
+                exports.getJSON(GEToptions(api.url, req.params.path, req.query.q, req.params.fields),
+                    function (statusCode, result) {
+                        console.log("onResult: (" + statusCode + ")" + JSON.stringify(result));
+                        res.statusCode = statusCode;
+                        res.send(result);
+                    });
+            });
 
-                app.post('/' + api.id + '/:path', function (req, res) {
-                    var data = JSON.stringify(req.body);
+            app.put('/' + api.id + '/:path/:id', function (req, res) {
+                exports.postJSON(PUToptions(api.url, req.params.path, req.params.id, req.body),
+                    req.body,
+                    function (statusCode, result) {
+                        console.log("onResult: (" + statusCode + ")" + JSON.stringify(result));
+                        res.statusCode = statusCode;
+                        res.send(result);
+                    });
+            });
 
-                    console.log('post');
-                    console.log(data);
-                    console.log(req.body);
-                    exports.postJSON(options(api.url, req.params.path, 'POST', data),
-                        data,
-                        function (statusCode, result) {
-                            res.statusCode = statusCode;
-                            res.send(result);
-                        });
-                });
+            app.post('/' + api.id + '/:path', function (req, res) {
+                exports.postJSON(POSToptions(api.url, req.params.path, req.body),
+                    req.body,
+                    function (statusCode, result) {
+                        res.statusCode = statusCode;
+                        res.send(result);
+                    });
+            });
 
-                app.delete('/' + api.id +'/:path(*)', function (req, res) {
-                    console.log(req);
-                    exports.deleteJSON(options(api.url, req.params.path, 'DELETE'),
-                        '',
-                        function (statusCode, result) {
-                            console.log("onResult: (" + statusCode + ")" + JSON.stringify(result));
-                            res.statusCode = statusCode;
-                            res.send(result);
-                        });
-                });
-            }
-        });
+            app.delete('/' + api.id +'/:path/:id', function (req, res) {
+                console.log(req);
+                exports.deleteJSON(DELETEoptions(api.url, req.params.path, req.params.id),
+                    '',
+                    function (statusCode, result) {
+                        console.log("onResult: (" + statusCode + ")" + JSON.stringify(result));
+                        res.statusCode = statusCode;
+                        res.send(result);
+                    });
+            });
+        }
     }
 }
